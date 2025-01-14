2b6d87a74257fd892fa1d1ba49f8eb53d4f02731
Shivakumar <shivu11022002@gmail.com>
2025-01-14 22:55:16 +0530

    ADDED NEW DATABASE

--- base/settings.py
+++ base/settings.py
@@ -118,10 +118,7 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
