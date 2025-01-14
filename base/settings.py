d580076cb505514db2d429059fc466b8b0f86a7a
Shivakumar <shivu11022002@gmail.com>
2025-01-14 22:54:20 +0530

    changed allowed host

--- base/settings.py
+++ base/settings.py
@@ -28,9 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'base-p5gg.onrender.com',
+    'slipwrite.com'
+    'localhost:5173'
+]
 
 
 # Application definition
