e08a2bb6d94a9053d396a6c158c5611c3deb9823
Shivakumar <shivu11022002@gmail.com>
2025-01-21 15:00:32 +0530

    changed db conf

--- base/settings.py
+++ base/settings.py
@@ -172,14 +172,15 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 if DEBUG:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
         'default': {
             "ENGINE": 'django.db.backends.sqlite3',
             "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
         }
+    }
+ 
+else:
+       DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 
 # Password validation
